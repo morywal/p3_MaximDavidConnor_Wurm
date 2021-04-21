e9c89bade1120efd37b825facee5bf5446519d1a
unsaf <unsaf@DESKTOP-QOE5IV5.attlocal.net>
2021-04-21 13:43:03 -0700

    gamer

--- snake/src/MainPanel.java
+++ snake/src/MainPanel.java
@@ -1,9 +1,17 @@
 import java.awt.Color;
 import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MainPanel extends JPanel{
+	
+	ImageIcon menu = new ImageIcon("gamer");
+	JLabel first;
+	
+	first = new JLabel("Gamer",menu, SwingConstants.CENTER);
 	public void paint(Graphics g) {
 		
 	}
@@ -12,9 +20,9 @@ public class MainPanel extends JPanel{
 		
 		
 		JFrame j = new JFrame();
-		j.setSize(600, 600);
+		j.setSize(1200, 720);
 		Color b = new Color(255,255,255);
-		
+		j.add(first)
 		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
 		j.setResizable(false);
 		j.setBackground(b);
