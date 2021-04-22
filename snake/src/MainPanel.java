cf19563967f42af3e82d35dffd201099ce3d4420
unsaf <unsaf@DESKTOP-QOE5IV5.attlocal.net>
2021-04-22 12:42:15 -0700

    hm

--- snake/src/MainPanel.java
+++ snake/src/MainPanel.java
@@ -1,19 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MainPanel extends JPanel{
+public class MainPanel extends JPanel implements MouseListener, ActionListener{
 	
-	ImageIcon menu = new ImageIcon("gamer");
-	JLabel first;
-	
-	first = new JLabel("Gamer",menu, SwingConstants.CENTER);
+	Image mainMenu = new ImageIcon("/snake/Images/Gifs/gang gang.gif").getImage();
 	public void paint(Graphics g) {
-		
+		g.drawImage(mainMenu, 30, 30, this);
 	}
 	
 	public MainPanel() {
@@ -22,10 +24,45 @@ public class MainPanel extends JPanel{
 		JFrame j = new JFrame();
 		j.setSize(1200, 720);
 		Color b = new Color(255,255,255);
-		j.add(first);
 		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
 		j.setResizable(false);
 		j.setBackground(b);
 		j.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
